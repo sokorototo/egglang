1af360d811a3184f610a44e878f408be360f6a37
sokorototo <nyachiengatoto@gmail.com>
2022-07-04 21:58:25 +0300

    Parser from logos made into main parser

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,6 @@ mod parser;
 mod scope;
 mod special;
 
-// Simple timing utility
-fn time<T>(tag: &str, f: impl FnOnce() -> T) -> T {
-    let start = std::time::Instant::now();
-    let result = f();
-
-    println!("Execution [{tag}] took: {:?}", start.elapsed());
-    result
-}
-
 fn main() {
     // Read data
     let code = {
@@ -26,33 +17,14 @@ fn main() {
         read_to_string(path).unwrap()
     };
 
-    // Defined regexi
-    let regexi = &[
-        // String
-        regex::Regex::new("^\"([^\"]*)\"").unwrap(),
-        // Number
-        regex::Regex::new(r"^(\+|-)?\d+\b").unwrap(),
-        // Word
-        regex::Regex::new(r#"^[^\s\(\),#"]+"#).unwrap(),
-        // Comment
-        regex::Regex::new(r"^#.*\n\s+").unwrap(),
-    ];
-
     // Parse the expression
-    // let expr = time("Regex Tokenization", || parser::parse(&code, regexi));
-    let exprs = time("Logos Tokenization", || parser::logos_parser::parse(code));
+    let exprs = parser::parse(code);
 
     // Define runtime variables
     let mut scope = scope::build_default_scope();
     let special_forms = special::build_special_forms();
 
-    // Log outputs
-    std::fs::write("logos.txt", format!("{:#?}", &exprs[0])).unwrap();
-    // std::fs::write("regex.txt", format!("{expr:#?}")).unwrap();
-
-    let result = time("Expression evaluation", || {
-        evaluator::evaluate(&exprs[0], &mut scope, &special_forms)
-    });
+    let result = evaluator::evaluate(&exprs[0], &mut scope, &special_forms);
 
     println!("Result of evaluation: {result:?} ");
 }
--- src/parser.rs
+++ src/parser.rs
@@ -1,96 +1,100 @@
-pub mod logos_parser;
+use logos::Logos;
+
 use crate::expression::{Expression, Value};
 
-// Parse a string into an expression
-pub fn parse<S: AsRef<str>>(source: S, regexi: &[regex::Regex; 4]) -> Expression {
-    let (expr, _) = parse_expression(source.as_ref(), regexi);
-    expr
+#[derive(logos::Logos, Debug, PartialEq)]
+enum Token {
+    #[regex("\"([^\"\n])*\"")]
+    String,
+    #[regex(r"(\+|-)?\d+", priority = 2)]
+    Number,
+    #[regex(r#"[^\s\(\),#"]+"#)]
+    Word,
+
+    #[regex(r"\(")]
+    LeftBracket,
+    #[regex(r"\)")]
+    RightBracket,
+
+    #[regex(",", logos::skip)]
+    Comma,
+    #[regex(r"\s+", logos::skip)]
+    WhiteSpace,
+    #[regex(r"#.*(\s)*", logos::skip)]
+    Comment,
+
+    #[error]
+    UnknownToken,
 }
 
-// Given an expression, returns the token type
-fn parse_expression<'t>(source: &'t str, regexi: &[regex::Regex; 4]) -> (Expression, &'t str) {
-    let mut source = source.trim_start();
+pub fn parse<S: AsRef<str>>(script: S) -> Vec<Expression> {
+    let source = script.as_ref();
+    let tokens = tokenize(source);
+    let expressions = parse_tokens(tokens.as_slice());
 
-    // Get regex definitions
-    let string_regex = &regexi[0];
-    let number_regex = &regexi[1];
-    let word_regex = &regexi[2];
-
-    // Trims comments
-    let comment_regex = &regexi[3];
-    while let Some(captures) = comment_regex.captures(source) {
-        source = &source[captures[0].len()..];
-    }
-
-    let (expr, len) = if let Some(captures) = string_regex.captures(source) {
-        let str = &captures[1];
-        let full = &captures[0];
-
-        (
-            Expression::Value {
-                value: Value::String(str.into()),
-            },
-            full.len(),
-        )
-    } else if let Some(captures) = number_regex.captures(source) {
-        let str = &captures[0];
-
-        (
-            Expression::Value {
-                value: Value::Number(str.parse().unwrap()),
-            },
-            str.len(),
-        )
-    } else if let Some(captures) = word_regex.captures(source) {
-        let str = &captures[0];
-
-        (Expression::Word { name: str.into() }, str.len())
-    } else {
-        panic!("Syntax error! Unknown syntax {}", source)
-    };
-
-    parse_operations(expr, &source[len..], regexi)
+    expressions
 }
 
-// Given a stream of characters, separates characters into valid chunks to be parsed into expressions
-fn parse_operations<'t>(
-    expr: Expression,
-    source: &'t str,
-    regexi: &[regex::Regex; 4],
-) -> (Expression, &'t str) {
-    let source = source.trim_start();
+fn tokenize(code: &str) -> Vec<(Token, String)> {
+    let mut lex = Token::lexer(code);
+    let mut tokens = Vec::new();
 
-    // Is this an application?
-    if !source.starts_with('(') {
-        return (expr, source);
+    while let Some(token) = lex.next() {
+        tokens.push((token, lex.slice().to_string()))
     }
 
-    // Remove the leading '('
-    let mut source = &source[1..];
+    tokens
+}
 
-    // Gather arguments
-    let mut args = vec![];
+fn parse_tokens(tokens: &[(Token, String)]) -> Vec<Expression> {
+    let mut expressions = Vec::new();
+    let mut stack = vec![];
 
-    while !source.starts_with(')') {
-        let (expression, rest) = parse_expression(source, regexi);
-
-        args.push(expression);
-        source = rest.trim_start();
-
-        // trim ',' get second param
-        if source.starts_with(',') {
-            source = source[1..].trim();
-        } else if !source.starts_with(')') {
-            dbg!(source);
-            panic!("Expected a ',' or a ')'")
+    // Iterate over tokens
+    tokens.iter().for_each(|(token, data)| match token {
+        Token::String => {
+            let str = data.as_str();
+            expressions.push(Expression::Value {
+                value: Value::String(str[1..str.len() - 1].into()),
+            })
         }
-    }
+        Token::Number => expressions.push(Expression::Value {
+            value: Value::Number(data.parse().expect("Unable to parse string as number")),
+        }),
+        Token::Word => expressions.push(Expression::Word {
+            name: data.as_str().into(),
+        }),
 
-    // Build new application
-    let expr = Expression::Operation {
-        operator: Box::new(expr),
-        operands: args,
-    };
+        Token::LeftBracket => stack.push(expressions.len()),
+        Token::RightBracket => {
+            let start = stack.pop().expect("Unbalanced brackets");
+            let end = expressions.len();
+            let count = end - start;
 
-    parse_operations(expr, &source[1..], regexi)
+            // Collect operation arguments
+            let mut sub_expressions = Vec::with_capacity(count);
+
+            for i in start..end {
+                sub_expressions.push(expressions.get(i).expect("Unbalanced braces").clone());
+            }
+
+            // Truncate original expressions
+            expressions.truncate(expressions.len() - count);
+
+            // Get name of operation
+            let name = expressions.pop().expect("Unbalanced brackets");
+            let operation = Expression::Operation {
+                operator: Box::new(name),
+                operands: sub_expressions,
+            };
+
+            // Push operation to stack
+            expressions.push(operation);
+        }
+
+        Token::UnknownToken => panic!("Experienced unknown token in token stream: {data}"),
+        _ => unreachable!("Tokens are automatically filtered out by logos"),
+    });
+
+    expressions
 }
