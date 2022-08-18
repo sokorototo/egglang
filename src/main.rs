6c1bf084bb61242d4986ed727ed21cf579b76c9b
sokorototo <nyachiengatoto@gmail.com>
2022-08-18 22:11:02 +0300

    .

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,6 @@ pub mod operators;
 pub mod parser;
 pub mod scope;
 
-fn time<T>(description: &str, f: impl FnOnce() -> T) -> T {
-    let timer = std::time::Instant::now();
-    let res = f();
-
-    let elapsed = timer.elapsed().as_micros();
-    println!("{} took {}us", description, elapsed);
-    res
-}
-
 fn main() {
     // Read data
     let (code, path) = {
@@ -31,10 +22,13 @@ fn main() {
     let mut scope = scope::new();
     let builtins = operators::builtins();
 
-    time(format!("{path}").as_str(), || {
-        exprs
-            .iter()
-            .map(|expr| evaluator::evaluate(expr, &mut scope, &builtins))
-            .for_each(drop)
-    });
+    {
+        let timer = std::time::Instant::now();
+
+        for expr in exprs.as_slice() {
+            evaluator::evaluate(&expr, &mut scope, &builtins);
+        }
+
+        println!("{} took {}us", path, timer.elapsed().as_micros());
+    };
 }
